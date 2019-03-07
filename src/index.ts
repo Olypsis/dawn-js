6349577cdf2bb7a430d8f45fa772070be7df7e9c
sharad-s <sharadshekar@gmail.com>
2019-03-07 15:14:05 -0600

    fix(status): cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,36 @@
-import { connectStatus, newStatus } from './status';
 import StatusJS = require('status-js-api');
 
-const testProvider = 'http://35.188.163.32:8545';
-
 export class Dawn {
-  public statusJS?: StatusJS;
+  public statusJS: StatusJS;
   public statusPublicKey?: string;
   public statusUsername?: string;
+  private testStatusProvider: string = 'http://35.188.163.32:8545';
+
+  constructor() {
+    this.statusJS = new StatusJS();
+    console.log('Dawn and Status initialized');
+  }
 
   public async connect(
-    statusProvider: string = testProvider,
+    statusProvider: string = this.testStatusProvider,
     privateKey?: string,
   ): Promise<boolean> {
     try {
-      // Instantiate Status
-      this.statusJS = newStatus();
-      console.log('Status initialized');
-
       // Connect to Status Provider
       await this.statusJS.connect(statusProvider, privateKey);
 
-      // Get Public Key
+      // Get Public Key and Username
       this.statusPublicKey = await this.statusJS.getPublicKey();
       this.statusUsername = await this.statusJS.getUserName();
-
       console.log(
-        `Connected to Status as ${this.statusUsername}. \nPublic Key: ${
+        `Connected to Status as ${this.statusUsername}. Public Key: ${
           this.statusPublicKey
         }`,
       );
+
       return true;
     } catch (err) {
-      // Something failed
+      // Something failed during connection
       console.log(err.message);
       return false;
     }
